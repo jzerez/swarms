b1392bcfd07eba564e10b053843f8c27cb12fc92
Nathan Estill <nathan.estill@students.olin.edu>
2020-11-23 17:53:24 -0500

    adding a little bit more code for the robots stuff

--- Simulator.py
+++ Simulator.py
@@ -2,34 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 import rd
+from robot import Robot
+from matplotlib.animation import FuncAnimation
 
 class Simulator(object):
     """docstring for Simulator."""
 
-    def __init__(self, n_steps, grid_size, rd_params, side_length):
-        self.a, self.b = rd.reaction_diffusion(n_steps, grid_size, rd_params)
+    def __init__(self, nSteps, gridSize, rdParams, sideLength, shape="circle"):
+        # self.a, self.b = rd.reaction_diffusion(n_steps, grid_size, rd_params)
+        self.rdParams = rdParams
+        self.nSteps = nSteps
+        self.gridSize = gridSize
+        self.sideLength = sideLength
+        self.grid = self.initGrid()
 
-        self.robots = self.init_robots()
-
-    def init_robots(self):
+    def initGrid(self):
         "returns a list of Robot objects. Based on a truncated view of self.a"
-        pass
+        offset = (self.gridSize - self.sideLength) // 2
+        robots = np.array([[Robot(0.5,0.1,x + offset,y + offset,self.rdParams) for x in range(self.sideLength)] for y in range(self.sideLength)])
 
-    def calc_neighbors(self, robot):
+        robots[self.sideLength//2,self.sideLength//2].a += 0.1
+
+        grid = np.zeros((self.gridSize, self.gridSize), dtype=object)
+        firstInd = offset
+        secondInd = firstInd + self.sideLength
+        grid[firstInd:secondInd, firstInd:secondInd] = robots
+        self.grid = grid
+
+    def calcNeighbors(self, robot):
         "finds the visible neighbors of a robot. No return value"
-        neighbors = None
-        robot.set_neighbors(neighbors)
-        pass
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        neighbors = [self.grid[robot.x+x][robot.y+y] for x,y in directions if isinstance(self.grid[robot.x+x][robot.y+y], Robot)]
+        robot.setNeighbors(neighbors)
 
-    def update_simulation(self):
-        for robot in self.robots:
-            self.calc_neighbors(robot)
-            robot.move()
+    def updateSimulation(self):
+        for gridSpace in self.grid.flatten():
+            if(isinstance(gridSpace,Robot)):
+                self.calcNeighbors(gridSpace)
+                gridSpace.updateChemicals()
+            # Todo: robot.move()
+        plt.imshow(self.plottableGrid(self.grid))
+
+    def plottableGrid(self, grid):
+        plotGrid = grid
+        plotGrid[np.where(not isinstance(grid, Robot))] = -1
+        return plotGrid
+
+    def initPlot(self):
+        plt.imshow(self.plottableGrid(self.grid))
+
 
     def main(self):
-        while(True):
-            self.update_simulation()
+        fig = plt.figure()
+        anim = FuncAnimation(fig, self.updateSimulation, init_func=self.initPlot, frames=20, repeat=False)
+        plt.show()
+        # while(True):
+        #     self.updateSimulation()
 
 if __name__ == '__main__':
-    sim = Simulator()
+    sim = Simulator(nSteps = 1000, gridSize=100, rdParams=[0.2,0.5,0.055,-0.117],sideLength=50)
     sim.main()
--- robot.py
+++ robot.py
@@ -2,26 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 
-
 class Robot(object):
     """docstring for Robot."""
 
-    def __init__(self,a,b,x,y,neighbor_radius,rd_params):
+    def __init__(self,a,b,x,y,rd_params): # neighborRadius
         self.a = a
         self.b = b
-        self.rd_params = rd_params
-        self.neighbor_radius = neighbor_radius
+        self.ca, self.cb, self.a_add_rate, self.b_add_rate = rd_params
+        # self.neighborRadius = neighborRadius
         self.pos = np.array([[x, y]])
 
-    def detect_edge(self):
+    def detectEdge(self):
         "returns true if agent is on edge"
         pass
 
-    def set_neighbors(self,neighbors):
+    def setNeighbors(self,neighbors):
         "sets the internal neighbors"
         self.neighbors = neighbors
 
-    def calc_moves(self):
+    def updateChemicals(self):
+        # later we can scale the diffusion by the distance?
+        neighborMeanA = np.mean([neighbor.a for neighbor in self.neighbors])
+        neighborMeanB = np.mean([neighbor.b for neighbor in self.neighbors])
+        reaction = a * b**2
+        self.a += (-self.a + neighborMeanA) * self.ca - reaction + self.a_add_rate * (1-a)
+        self.b += (-self.b + neighborMeanB) * self.cb + reaction + self.b_add_rate * b
+
+    def getLEDValue(self):
+        # finish this when we plot, map from a to col
+        return self.a
+
+    def calcMoves(self):
         "returns list of empty squares to move to"
 
     def move(self):
