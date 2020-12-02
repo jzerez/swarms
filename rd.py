b5706318e27ef11271e4e189af9f9cbf77397674
jzerez <Jonathan.zerez@students.olin.edu>
2020-12-02 11:14:43 -0500

    fixed rd

--- rd.py
+++ rd.py
@@ -6,20 +6,22 @@ import scipy.signal
 
 
 def reaction_diffusion(n_steps, grid_size, ca=0.2, cb=0.25, a_add_rate=0.055, b_add_rate=-0.118, plot_on=True):
-    kernel = np.array([[0, 1, 0],
-                       [1, -4, 1],
-                       [0, 1, 0]])
+    kernel = np.array([[.05, .2, .05],
+                       [ .2, -1, .2],
+                       [.05, .2, .05]])
 
-    a = 0.5 * np.ones(grid_size, dtype=float)
+    a = np.ones(grid_size, dtype=float)
     b = 0.1 * np.random.random(grid_size)
 
+    spx = grid_size[0]//2
+    spy = grid_size[1]//2
+    r=grid_size[0] // 20
+    b[spx-r:spx+r, spy-r:spy+r] += 0.1
 
-    a[grid_size[0]//2, grid_size[1]//2] += 0.1
-
-    if plot_on:
-        plt.figure()
-        plt.imshow(a)
-        plt.colorbar()
+    # if plot_on:
+    #     plt.figure()
+    #     plt.imshow(a)
+    #     plt.colorbar()
 
     for step in range(n_steps):
         div_a = scipy.signal.correlate2d(a, kernel, mode='same')
@@ -27,7 +29,7 @@ def reaction_diffusion(n_steps, grid_size, ca=0.2, cb=0.25, a_add_rate=0.055, b_
         reaction = a * b**2
         a += div_a * ca - reaction + a_add_rate * (1-a)
         b += div_b * cb + reaction + b_add_rate * b
-        print(np.max(np.max(a)), np.max(np.max(b)))
+        # print(np.max(np.max(a)), np.max(np.max(b)))
 
     if plot_on:
         plt.figure()
@@ -36,7 +38,10 @@ def reaction_diffusion(n_steps, grid_size, ca=0.2, cb=0.25, a_add_rate=0.055, b_
         title = str(n_steps) + ' time steps'
         plt.title(title)
 
-        plt.show()
+        # plt.show()
 
 if __name__ == '__main__':
-    reaction_diffusion(200, (200,200), plot_on=True)
+    for nsteps in [4500]:
+        reaction_diffusion(nsteps, (100,100), ca=0.4, cb=0.2, a_add_rate=0.039, b_add_rate=-0.104, plot_on=True)
+        # reaction_diffusion(nsteps, (100,100), ca=0.4, cb=0.2, a_add_rate=0.055, b_add_rate=-0.117, plot_on=True)
+    plt.show()
